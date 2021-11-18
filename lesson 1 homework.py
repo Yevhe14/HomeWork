27d56ae2c464b1730ea34b93138eddf9a41db015
Yevhen Bilokin <zenyen14@gmail.com>
2021-11-18 14:25:09 +0200

    д/з урок 1

---
+++ lesson 1 homework.py
@@ -0,0 +1,14 @@
+#task 1
+print("task 1")
+strona1 = int(input("Введіть довжину 1 сторони трикутника в см: "))
+strona2 = int(input("Введіть довжину 2 сторони трикутника в см: "))
+strona3 = int(input("Введіть довжину 3 сторони трикутника в см: "))
+
+print("Периметр трикутника = ",strona1 + strona2 + strona3)
+
+#task 2
+print("task 2")
+town = str(input("Введіть з якого ви міста: "))
+numSchool = int(input("Введіть номер школи: "))
+
+print(f"Привіт. Я з {town}, навчаюся у школі №{numSchool}.")
